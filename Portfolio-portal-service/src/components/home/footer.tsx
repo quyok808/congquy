11b974a796daaf82da95dce870a41215ad7a3ded
Cong Quy <ncquy250203@gmail.com>
2025-12-06 13:50:59 +0700

    add qr navigate

--- Portfolio-portal-service/src/components/home/footer.tsx
+++ Portfolio-portal-service/src/components/home/footer.tsx
@@ -1,7 +1,9 @@
 import { Github, Linkedin, Mail } from "lucide-react";
 import { EMAIL, GITHUB, LINKEDIN } from "../../constants/home.constants";
+import { useNavigate } from "react-router-dom";
 
 export function Footer() {
+  const navigator = useNavigate();
   return (
     <footer className="bg-background border-t py-12">
       <div className="container mx-auto px-4">
@@ -30,6 +32,22 @@ export function Footer() {
               }}
             >
               <Mail className="h-5 w-5" />
+            </button>
+          </div>
+
+          <div id="qr" className="flex justify-center space-x-6 mb-6">
+            <button
+              className="px-4 py-2 rounded-lg bg-black/40 text-dark dark:bg-white/10 dark:text-white hover:bg-black/60 transition"
+              onClick={() => navigator("/qrCode")}
+            >
+              QR Code
+            </button>
+
+            <button
+              className="px-4 py-2 rounded-lg bg-black/40 text-dark dark:bg-white/10 dark:text-white hover:bg-black/60 transition"
+              onClick={() => navigator("/viewqr")}
+            >
+              View QR
             </button>
           </div>
 
